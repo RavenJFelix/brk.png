5d8ee1d2b3f0114f036991cde9d5666cb57bf476
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-11-06 21:46:03 -0800

    Write a stupid header.

---
+++ brick-hill-png.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""brick-hill-png.py: This glorious, absolutely amazinng and wonderful program converts"""
+"""An image into a brk file."""
+
+__author__ = "Raven Jyro \"G14989\" Felix"
+__copyright__ = "MIT"
+
+
