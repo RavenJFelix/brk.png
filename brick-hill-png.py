a61164640f6f443eb09c4d804a0a160542cadd5c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-11-06 23:57:47 -0800

    Add file arguments

--- brick-hill-png.py
+++ brick-hill-png.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 """brick-hill-png.py: This glorious, absolutely amazinng and wonderful program converts"""
 """An image into a brk file."""
 
@@ -9,6 +10,8 @@ __copyright__ = "MIT"
 IMAGE_HEIGHT = 5
 
 from PIL import Image 
+import sys
+
 MAX_8_BIT = 255
 
 def percentize_tuple(le_tuple, max_value):
@@ -20,9 +23,6 @@ def percentize_tuple(le_tuple, max_value):
 def _test_percentize_tuple():
     """If the tuple is of 8 bit, then max_value is 255
         If a value is 255 it becomes 1."""
-    
-    print("asdf")
-
     test_tuple = (255, 221, 234)
     expected_tuple = (255/255, 221/255, 234/255)
     calculated_tuple = percentize_tuple(test_tuple, 255)
@@ -36,7 +36,6 @@ def _test_percentize_tuple():
     test_tuple = (55, 21, 234, 0)
     expected_tuple = (255/255, 221/255, 234/255, 0/255)
     calculated_tuple = percentize_tuple(test_tuple, 255)
-    print(calculated_tuple)
     assert not (calculated_tuple == expected_tuple)
 
 def generate_brk_header():
@@ -90,15 +89,18 @@ def _test():
 
 _test()
 
-file_name = "roblox-logo.png"
-f = open(file_name + ".brk", "a")
+# file_name = "smolkek.png"
+print(sys.argv[1])
+file_name = sys.argv[1]
+f = open(file_name + ".brk", "w+")
 img = Image.open(file_name).convert(mode="RGBA")
 
-f.write(generate_brk_header() + "\n\n")
+f.write(generate_brk_header() + "\n")
 
 for x in range(img.width):
     for y in range(img.height):
-        color = image.get_pixel(x, y)
+        color = img.getpixel((x, y))
         position = (x, y, IMAGE_HEIGHT)
-        f.write(generate_brk_pixel_brick(position, color)
+        f.write(generate_brk_pixel_brick(position, color) + "\n")
 
+f.close()
