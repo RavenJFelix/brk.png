eb536b72aeba1da9216034ca5e3591063caa048c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-11-06 23:05:28 -0800

    Add Tests

--- brick-hill-png.py
+++ brick-hill-png.py
@@ -38,15 +38,49 @@ def _test_percentize_tuple():
     print(calculated_tuple)
     assert not (calculated_tuple == expected_tuple)
 
+def generate_brk_header():
+    return (
+    "B R I C K  W O R K S H O P  V0.2.0.0\n"
+    "\n"
+    "0 0 0\n"
+    "0.14 0.51 0.20 1\n"
+    "0.49 0.70 0.90\n"
+    "100\n"
+    "400\n")
+
+def _test_generate_brk_header():
+    1
+
+def list_to_space_separated(array):
+    output = ""
+    for element in array:
+        output += str(element) + " "
+    return output[:-1] # Cut the superfluous space at the end
+
+def _test_list_to_space_separated():
+    test_array = [1, 2, 3, 4]
+    expected = "1 2 3 4"
+    output = list_to_space_separated(test_array)
+    assert expected == output
+
+def generate_brk_pixel_brick(position_tuple, color_rgba, name="Sharko"):
+
+def _test_generate_brk_pixel_brick():
+    color_raw = (23, 43, 243)
+    color = percentize_tuple(color_raw, MAX_8_BIT)
+    expected_value = "1 2 3 1 1 1"
+
 
 def _test():
     _test_percentize_tuple()
+    _test_generate_brk_header()
+    _test_list_to_space_separated()
 
 _test()
 
-print("Frick")
+print(generate_brk_header())
 img = Image.open("roblox-logo.png").convert(mode="RGBA")
-print(img.width)
 for x in range(img.width):
     for y in range(img.height):
+        1
         #print("img.getpixel((x, y))")
